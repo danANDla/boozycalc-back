27810947db2b63325de91f1bfdfb5aaf571a8736
archdla <d.a.n.202021@gmail.com>
2022-07-28 17:31:48 +0300

    initial commit

---
+++ src/main/java/com/danandla/boozycalc/BoozycalcAppApplication.java
@@ -0,0 +1,13 @@
+package com.danandla.boozycalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoozycalcAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoozycalcAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danandla/boozycalc/BoozycalcAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danandla.boozycalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoozycalcAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
